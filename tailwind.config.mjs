88024d55ccd8529b2873d02050dbca9dcea0cdcb
Jero <jeruah@unal.edu.co>
2024-10-23 13:00:38 -0500

    Photo album and animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,13 @@ import animations from '@midudev/tailwind-animations';
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#fec3ff',
+				secondary: '#000',
+				white: '#fff',
+			}
+		},
 	},
 	plugins: [animations],
 }
