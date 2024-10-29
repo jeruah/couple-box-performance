92057ebfed60fd2f7af9a87dd1dc4f87d9d4cf8a
Jero <jeruah@unal.edu.co>
2024-10-29 16:38:52 -0500

    dynamic post control, dependencies updated, and form for post added

---
+++ src/post.ts
@@ -0,0 +1,15 @@
+const imgInfo = [
+    {
+        id: "el-dia-que-nos-conocimos",
+        src: "https://picsum.photos/800/600?random=1",
+        alt: "El dia que nos conocimos",
+        post: {
+            title: "EL dia que nos conocimos",
+            music: "https://open.spotify.com/intl-es/track/2UZZtkoLOg7IHxeTAdPFvd?si=7cf9e43fb3b6469a",
+            content:
+                "Lorem	Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        },
+    },
+];
+
+export default imgInfo;
